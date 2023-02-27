c50480dfa3c07ee708b876098545ca98156667cc
Pejman Hadavi <pejmanhadaviph@yahoo.com>
2023-02-27 11:16:11 +0330

    Add results of user inputs.

--- src/components/verses-diolag.jsx
+++ src/components/verses-diolag.jsx
@@ -11,16 +11,17 @@ import IconButton from '@mui/material/IconButton';
 import Typography from '@mui/material/Typography';
 import CloseIcon from '@mui/icons-material/Close';
 import Slide from '@mui/material/Slide';
+import TextField from '@mui/material/TextField';
+import Box from '@mui/material/Box';
 
 const Transition = React.forwardRef(function Transition(props, ref) {
     return <Slide direction="up" ref={ref} {...props} />;
 });
 
-export default function FullScreenDialog({open, handleClose, verses}) {
+export default function FullScreenDialog({ open, handleClose, verses, inputData }) {
 
     return (
         <div>
-
             <Dialog
                 fullScreen
                 open={open}
@@ -38,26 +39,74 @@ export default function FullScreenDialog({open, handleClose, verses}) {
                             <CloseIcon />
                         </IconButton>
                         <Typography sx={{ ml: 2, flex: 1 }} variant="h6" component="div">
-                            Sound
+                            Found {verses.length} Results
                         </Typography>
-                        <Button autoFocus color="inherit" onClick={handleClose}>
-                            save
-                        </Button>
                     </Toolbar>
                 </AppBar>
+                <Box
+                    sx={{
+                        '& .MuiTextField-root': { mt: 4, ml: 1, mr: 1, width: '25ch' },
+                    }}
+                    noValidate
+                    autoComplete="off"
+                >
+                    <div>
+                        <TextField
+                            disabled
+                            label="Main roots"
+                            defaultValue={inputData.mainRoots}
+                        />
+                        <TextField
+                            disabled
+                            label="First ignored chars"
+                            defaultValue={inputData.firstIgnoredChars.length ? inputData.firstIgnoredChars : 'Not set'}
+                        />
+                        <TextField
+                            disabled
+                            label="Last ignored chars"
+                            defaultValue={inputData.lastIgnoredChars.length ? inputData.lastIgnoredChars : 'Not set'}
+                        />
+                        <TextField
+                            disabled
+                            label="Max length"
+                            defaultValue={inputData.wordMaxLength || 'Not set'}
+                        />
+                        <TextField
+                            disabled
+                            label="Similarity percent"
+                            defaultValue={inputData.similarityPercent || 'Not set'}
+                        />
+                        <TextField
+                            disabled
+                            label="Should starts withRoots"
+                            defaultValue={inputData.shouldStartsWithRoots ? 'On' : 'Off'}
+                        />
+                        <TextField
+                            disabled
+                            label="Could other chars exst"
+                            defaultValue={inputData.couldOtherCharsExistBetweenRootsChars ? 'On' : 'Off'}
+                        />
+                        <TextField
+                            disabled
+                            label="Use similarity"
+                            defaultValue={inputData.useSimilarity ? 'On' : 'Off'}
+                        />
+                    </div>
+                </Box>
                 <List>
                     {
                         verses.map(item => {
                             const address = item.substring(0, 16);
                             const verse = item.substring(16, item.length);
                             return (
-                            <React.Fragment dir="rtl">
-                                <ListItem>
-                                    <ListItemText primary={verse} secondary={address}/>
-                                </ListItem>
-                                <Divider />
-                            </React.Fragment>
-                        )})
+                                <React.Fragment dir="rtl">
+                                    <ListItem>
+                                        <ListItemText primary={verse} secondary={address} />
+                                    </ListItem>
+                                    <Divider />
+                                </React.Fragment>
+                            )
+                        })
                     }
                 </List>
             </Dialog>
