71202cb405d0d39df105d041419dca1b9181b9b0
Pejman Hadavi <pejmanhadaviph@yahoo.com>
2023-02-27 14:58:33 +0330

    Add list of holy books.

---
+++ src/data/list-of-books.js
@@ -0,0 +1,45 @@
+export const listOfBooks = `
+001 GEN	Genesis	براشیت
+002 EXO	Exodus	شموت
+003 LEV	Leviticus	ویقرا
+004 NUM	Numbers	بمدبر
+005 DEU	Deuteronomy	دبریم
+006 JOS	Joshua	یهوشع
+007 JDG	Judges	شوفطیم
+008 RUT	Ruth	روت
+009 1SA	1 Samuel	شموال ا
+010 2SA	2 Samuel	شموال ب
+011 1KI	1 Kings	ملکیم ا
+012 2KI	2 Kings	ملکیم ب
+013 1CH	1 Chronicles	دبری هیمیم ا
+014 2CH	2 Chronicles	دبری هیمیم ب
+015 EZR	Ezra	عزرا
+016 NEH	Nehemiah	نحمیه
+017 EST	Esther	استر
+018 JOB	Job	ایوب
+019 PSA	Psalms	تهیلیم
+020 PRO	Proverbs	مִشְׁلֵی
+021 ECC	Ecclesiastes	قֹهֶلֶت
+022 SOL	Song of Solomon	شیر هشیریم
+023 ISA	Isaiah	یشعه
+024 JER	Jeremiah	یرمیه
+025 LAM	Lamentations	ایکه
+026 EZE	Ezekiel	یحزقال
+027 DAN	Daniel	دنیال
+028 HOS	Hosea	هوشع
+029 JOE	Joel	یوال
+030 AMO	Amos	عموس
+031 OBA	Obadiah	عبدیه
+032 JON	Jonah	یونه
+033 MIC	Micah	میکه
+034 NAH	Nahum	نحوم
+035 HAB	Habakkuk	حبقوق
+036 ZEP	Zephaniah	صفنیه
+037 HAG	Haggai	حجی
+038 ZEC	Zechariah	زکریه
+039 MAL	Malachi	ملاکی
+040 MAT	Matthew	متی
+041 MAR	Mark	مرقوس
+042 LUK	Luke	لوقوس
+043 JOH	John	یوحنن
+044 QUR	Quran	قرآن`;
