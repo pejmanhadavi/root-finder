3a615794c419e2dd6d4a4c2b0e55a4a319ec0fb9
Pejman Hadavi <pejmanhadaviph@yahoo.com>
2023-03-01 00:56:24 +0330

    Fix avatar icon.

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -1,13 +1,13 @@
 import { Fragment } from "react";
 import Typography from '@mui/material/Typography';
-import LockOutlinedIcon from '@mui/icons-material/Book';
+import BookIcon from '@mui/icons-material/Book';
 import Avatar from '@mui/material/Avatar';
 
 export function Header() {
     return (
         <Fragment>
             <Avatar sx={{ m: 1, bgcolor: 'secondary.main' }}>
-                <LockOutlinedIcon />
+                <BookIcon />
             </Avatar>
             <Typography component="h1" variant="h5">
                 Find words Roots in Holy Books
