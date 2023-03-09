13d6f3b38f664e5c15eb132f1964e952a3c14a58
Pejman Hadavi <pejmanhadaviph@yahoo.com>
2023-03-09 11:32:07 +0330

    Change main component name.

--- src/App.jsx
+++ src/App.jsx
@@ -14,7 +14,7 @@ import { SearchForm } from './components/search-form';
 
 const theme = createTheme();
 
-export default function SignIn() {
+export default function App() {
 
   return (
     <ThemeProvider theme={theme}>
