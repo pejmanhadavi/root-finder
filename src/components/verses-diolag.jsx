e304d84cca7bac62583c79cde1e4635f8e906531
Pejman Hadavi <pejmanhadaviph@yahoo.com>
2023-03-09 17:14:20 +0330

    Add verse copier to application.

--- src/components/verses-diolag.jsx
+++ src/components/verses-diolag.jsx
@@ -1,6 +1,5 @@
 import * as React from 'react';
 import Dialog from '@mui/material/Dialog';
-import List from '@mui/material/List';
 import Divider from '@mui/material/Divider';
 import AppBar from '@mui/material/AppBar';
 import Toolbar from '@mui/material/Toolbar';
@@ -8,7 +7,7 @@ import IconButton from '@mui/material/IconButton';
 import Typography from '@mui/material/Typography';
 import CloseIcon from '@mui/icons-material/Close';
 import Slide from '@mui/material/Slide';
-import { Box } from '@mui/material';
+import { Box, Checkbox, FormGroup, FormControlLabel } from '@mui/material';
 import reactStringReplace from 'react-string-replace';
 
 const Transition = React.forwardRef(function Transition(props, ref) {
@@ -16,6 +15,27 @@ const Transition = React.forwardRef(function Transition(props, ref) {
 });
 
 export default function FullScreenDialog({ open, handleClose, verses, inputData }) {
+    const copiedVerses = [];
+
+    const handleCheckBoxOnChange = (e) => {
+        if (e.target.checked) {
+            copiedVerses.push(e.target.value)
+        } else {
+            if (copiedVerses.includes(e.target.value)) {
+                removeItemOnce(copiedVerses, e.target.value);
+            }
+        }
+        navigator.clipboard.writeText(copiedVerses)
+    };
+
+    function removeItemOnce(arr, value) {
+        var index = arr.indexOf(value);
+        if (index > -1) {
+          arr.splice(index, 1);
+        }
+        return arr;
+      }
+
     return (
         <div>
             <Dialog
@@ -35,29 +55,38 @@ export default function FullScreenDialog({ open, handleClose, verses, inputData 
                             <CloseIcon />
                         </IconButton>
                         <Typography sx={{ ml: 2, flex: 1 }} component="div">
-                            Found <span style={{ fontSize: '1.4rem' }}>{verses.length}</span> Results For:  <span style={{ fontSize: '1.4rem' }}>{inputData?.mainRoots?.join(',')}</span>
+                            Found <span style={{ fontSize: '1.4rem' }}>{verses.length}</span> Results
                         </Typography>
                     </Toolbar>
                 </AppBar>
-                <List>
+                <FormGroup>
                     {
                         verses.map((item, i) => {
                             return (
                                 <Box key={i} sx={{ m: 1 }}>
-                                    <p>{item.trueText}</p>
-                                    <p>{reactStringReplace(item.translitration, /\{(.*?)\}/, (match, i) => (
-                                        <span style={{ color: 'red', fontSize: '1.4rem' }}>{match}</span>
-                                    ))}</p>
-                                    <p>{reactStringReplace(item.translation, /\{(.*?)\}/, (match, i) => (
-                                        <span style={{ color: 'blue', fontSize: '1.4rem' }}>{match}</span>
-                                    ))}</p>
-                                    <p>{`${item.bookShortKey}, ${item.chapterNumber}, ${item.verseNumber}`}</p>
+                                    <FormControlLabel control={<Checkbox 
+                                    value={`
+${item.trueText}
+${item.translitration}
+${item.translation}
+${item.bookShortKey}, ${item.chapterNumber}, ${item.verseNumber}
+                                        `}
+                                    onChange={handleCheckBoxOnChange}
+                                    />}/>
+                                        <p>{item.trueText}</p>
+                                        <p>{reactStringReplace(item.translitration, /\{(.*?)\}/, (match, i) => (
+                                            <span style={{ color: 'red', fontSize: '1.4rem' }}>{match}</span>
+                                            ))}</p>
+                                        <p>{reactStringReplace(item.translation, /\{(.*?)\}/, (match, i) => (
+                                            <span style={{ color: 'blue', fontSize: '1.4rem' }}>{match}</span>
+                                        ))}</p>
+                                        <p>{`${item.bookShortKey}, ${item.chapterNumber}, ${item.verseNumber}`}</p>
                                     <Divider />
                                 </Box>
                             )
                         })
                     }
-                </List>
+                </FormGroup>
             </Dialog>
         </div>
     );
